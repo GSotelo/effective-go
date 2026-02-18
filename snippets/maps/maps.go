6e8bc006dd9d0f3192ea50d2f2048cc0abd11098
gsotelo <gsotelo.se@gmail.com>
2026-02-18 20:43:22 +0100

    Working on snippets

---
+++ snippets/maps/maps.go
@@ -0,0 +1,7 @@
+package maps
+
+func GenerateDogNames() map[string]string {
+	return map[string]string{
+		"briard": "Pedro Lain",
+	}
+}
---
+++ snippets/maps/maps_test.go
@@ -0,0 +1,8 @@
+package maps
+
+import "fmt"
+
+func ExampleGenerateDogNames() {
+	fmt.Print(GenerateDogNames()["briard"])
+	// Output: Pedro Lain
+}
